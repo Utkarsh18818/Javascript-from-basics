2a83ec196ddfce030e95da0f9150e26b149b8db1
Utkarsh Singh <utkarsh190707@gmail.com>
2024-08-25 06:00:50 +0000

    datatypes

---
+++ 02_datatypes/datatype.js
@@ -0,0 +1,34 @@
+// datatypes 
+
+// number => 2 to power 53.
+// big Int
+// float => 2.34, 3.54, 7.85 etc
+// string => " "
+// Boolean = true/false
+// null => stand alone value
+// undefined => assign nothing init
+// symbol => unique 
+
+// objects => stores values in the form of key value pairs
+
+// type of--
+//         |
+// example one
+console.log(typeof "Yuvraj")
+// example two here is something diffrent output when we put null inside it.
+console.log(typeof null)
+// here we see in output typeof null datatype is object.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
