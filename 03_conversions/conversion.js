4151761c5c54d83f33e7811082d0418064fdde7d
Utkarsh Singh <utkarsh190707@gmail.com>
2024-08-25 06:23:35 +0000

    conversions added

---
+++ 03_conversions/conversion.js
@@ -0,0 +1,34 @@
+let score = 33 ;
+console.log(typeof score)
+
+// here type of score is number.
+
+const valueinString = String(score)
+// here we convert it into string and assign it into valueinString
+
+console.log(typeof valueinString)
+// now it convert into string datatype it is called type conversion
+
+//******************************************************************************************************* */
+// Boolean type 
+
+let isLoggedin = 1;
+let ConvertintoBoolean = Boolean(isLoggedin)
+
+console.log(typeof ConvertintoBoolean);
+
+//##############################################################################################
+// String to  boolean
+
+let isname = "";
+let Convertinboolean = Boolean(isname)
+
+console.log(typeof Convertinboolean);
+
+// here we see whent we assign an empty string into it it gives us false and when we put some value init
+// it ll give us true
+
+// #####################################################################################################
+
+
+
